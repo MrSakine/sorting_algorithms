416a25e1e6cc2b0156011f263d7b3a82a9372e96
MrSakine <msakine20@gmail.com>
2023-11-17 05:17:46 +0000

    selection sort algorithm update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
